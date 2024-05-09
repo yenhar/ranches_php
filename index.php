5e734a85a630204af62989eec107e380a948fd15
Vincent Bryan <vbfcalija@yahoo.com>
2024-05-09 11:06:49 +0800

    Add displaying of users

--- index.php
+++ index.php
@@ -4,37 +4,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Registered Users</title>
     <style>
-        form div {
-            margin-bottom: 10px;
+        table {
+            margin-top: 2em;
         }
     </style>
+    <?php require('php/dbconfig.php'); ?>
 </head>
 
 <body>
-    <form action="php/register.php" method="POST">
-        <div>
-            <h1>Form Registration</h1>
-        </div>
-        <div>
-            <label for="first_name">First Name</label> <br />
-            <input type="text" id="first_name" name="first_name" placeholder="First name" autocomplete="false">
-        </div>
-        <div>
-            <label for="last_name">Last Name</label> <br />
-            <input type="text" id="last_name" name="last_name" placeholder="Last name" autocomplete="false">
-        </div>
-        <div>
-            <label for="gender">Gender</label> <br />
-            <input type="radio" id="gender" name="gender" value="Male"> Male
-            <input type="radio" id="genderFemale" name="gender" value="Female"> Female
-            <input type="radio" id="genderOthers" name="gender" value="Others"> Others
-        </div>
-        <div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
+    <h1>Users</h1>
+    <a href="registration.php">
+        <button>Register New User</button>
+    </a>
+    <table border="1" width="100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $sql = "SELECT id, first_name, last_name, gender FROM users";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" .
+                        $row["first_name"] . " " . $row["last_name"] .
+                        "</td>";
+                    echo "<td>" .
+                        $row["gender"] .
+                        "</td>";
+                    echo "<td>" .
+                        "<a href='edit.php?id=" . $row["id"] . "'>Edit</a>" .
+                        "<a href='php/delete-user.php?id=" . $row["id"] . "'>Delete</a>" .
+                        "</td>";
+                }
+            } else {
+                echo "0 results";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
---
+++ registration.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <style>
+        form div {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <form action="php/register.php" method="POST">
+        <a href="index.php">
+            <button>Back</button>
+        </a>
+        <div>
+            <h1>Form Registration</h1>
+        </div>
+        <div>
+            <label for="first_name">First Name</label> <br />
+            <input type="text" id="first_name" name="first_name" placeholder="First name" autocomplete="false">
+        </div>
+        <div>
+            <label for="last_name">Last Name</label> <br />
+            <input type="text" id="last_name" name="last_name" placeholder="Last name" autocomplete="false">
+        </div>
+        <div>
+            <label for="gender">Gender</label> <br />
+            <input type="radio" id="gender" name="gender" value="Male"> Male
+            <input type="radio" id="genderFemale" name="gender" value="Female"> Female
+            <input type="radio" id="genderOthers" name="gender" value="Others"> Others
+        </div>
+        <div>
+            <button type="submit">Register</button>
+        </div>
+    </form>
+</body>
+
+</html>
